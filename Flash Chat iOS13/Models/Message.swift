b2e4a92d388c116aba09f5553b95a0d45f958bb9
noureddineleg <58888748+noureddineleg@users.noreply.github.com>
2021-02-23 15:33:46 +0100

    Update Message.swift

--- Flash Chat iOS13/Models/Message.swift
+++ Flash Chat iOS13/Models/Message.swift
@@ -1,10 +1,3 @@
-//
-//  Message.swift
-//  Flash Chat iOS13
-//
-//  Created by Noureddine Legragui on 19/2/2021.
-//  Copyright © 2021 Angela Yu. All rights reserved.
-//
 
 import Foundation
 //create message model
